0c6ecc3548c5e2d59568b20844a17b896aade45b
Ian Oxley <ian.oxley@sage.com>
2018-09-18 08:52:51 +0100

    Run dts-gen to add boilerplate code
    Add boilerplate code, then remove the bits that aren't required.
    
    Rename tests file to tsx file

---
+++ types/carbon-react/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for carbon-react 4.7
+// Project: https://github.com/Sage/carbon#readme
+// Definitions by: Ian Oxley <https://github.com/ianoxley>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export * from './lib';
